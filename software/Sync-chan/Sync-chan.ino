07638c1e85573807e62d39d481d25192309b0b3e
Kaz Saita <saita@kinoshita-lab.org>
2020-03-24 23:31:38 +0900

    beautify

--- software/Sync-chan/Sync-chan.ino
+++ software/Sync-chan/Sync-chan.ino
@@ -72,6 +72,76 @@ enum ApplicationMode
 };
 ApplicationMode applicationMode = UNKNOWN_MODE;
 
+/***************************************
+ * Function Prototypes
+***************************************/
+// interrupt handlers
+void timer2Interrupt();
+void timerOneInterrupt();
+void externalSyncInterrupt();
+
+// state modifier and helpers
+void setStepParameter(StepParameter& s, const StepMode mode);
+void switchApplicationMode(const uint8_t newMode);
+void showStepMode(const int mode);
+void updateTempo7Seg(const uint16_t tempo);
+uint32_t bpmToSyncTimerCounter(const float bpm);
+
+// bpm calculation
+uint32_t getKnobBpm();
+float getFaderPercent();
+int nudge();
+
+// loops
+void internalMode_loop();
+void externalMode_loop();
+
+/***************************************
+ * Implementations
+***************************************/
+void timer2Interrupt()
+{
+    if (applicationMode == INTERNAL_MODE) {
+        digitalWrite(pin::Sync_Out, LOW);
+    }
+
+    MsTimer2::stop();
+}
+
+void timerOneInterrupt()
+{
+    if (applicationMode == INTERNAL_MODE) {
+        digitalWrite(pin::Sync_Out, HIGH);
+        Timer1.setPeriod(timerValue); // timer period must be set here to make "continuous" tempo output
+        MsTimer2::start();
+        return;
+    }
+
+    if (applicationMode == EXTERNAL_MODE) {
+        externalSyncNotCommingCounter++;
+        if (externalSyncNotCommingCounter >= stepParameter.notComingCounterMax) {
+            externalSyncNotCommingCounter = 0;
+            switchApplicationMode(INTERNAL_MODE);
+        }
+    }
+}
+
+void externalSyncInterrupt()
+{
+    externalSyncNotCommingCounter = 0;
+
+    if (applicationMode == INTERNAL_MODE) {
+        switchApplicationMode(EXTERNAL_MODE);
+    }
+
+    const auto pinState = digitalRead(pin::Ext_Sync);
+    digitalWrite(pin::Sync_Out, pinState);
+
+    if (pinState == HIGH) {
+        syncBpm.incomingPulse();
+    }
+}
+
 void setStepParameter(StepParameter& s, const StepMode mode)
 {
     switch (mode) {
@@ -87,7 +157,7 @@ void setStepParameter(StepParameter& s, const StepMode mode)
     }
 }
 
-void switchMode(const uint8_t newMode)
+void switchApplicationMode(const uint8_t newMode)
 {
     applicationMode = newMode;
 
@@ -111,31 +181,6 @@ void switchMode(const uint8_t newMode)
     digitalWrite(pin::Led_External, led_external);
 }
 
-void timer2Callback()
-{
-    if (applicationMode == INTERNAL_MODE) {
-        digitalWrite(pin::Sync_Out, LOW);
-    }
-
-    MsTimer2::stop();
-}
-
-void externalSyncInterrupt()
-{
-    externalSyncNotCommingCounter = 0;
-
-    if (applicationMode == INTERNAL_MODE) {
-        switchMode(EXTERNAL_MODE);
-    }
-
-    const auto pinState = digitalRead(pin::Ext_Sync);
-    digitalWrite(pin::Sync_Out, pinState);
-
-    if (pinState == HIGH) {
-        syncBpm.incomingPulse();
-    }
-}
-
 void showStepMode(const int mode)
 {
     switch (mode) {
@@ -152,37 +197,6 @@ void showStepMode(const int mode)
     delay(1000);
 }
 
-void setup()
-{
-    for (auto&& config : digitalPinConfigs) {
-        pinMode(config.pinNo, config.mode);
-    }
-
-    Serial.begin(115200);
-    auto mode = bootMode::getBootMode();
-    Serial.print("Boot Mode: ");
-    Serial.println(bootMode::bootModeText(mode));
-
-    if (mode == bootMode::INSPECTION) {
-        startHardwareInspection();
-        mode = bootMode::NORMAL_1STEP;
-    }
-    stepMode = mode == bootMode::NORMAL_1STEP ? STEP_1 : STEP_2;
-    setStepParameter(stepParameter, stepMode);
-    Serial.println(stepMode);
-    syncBpm.setMode(stepMode == STEP_1 ? SyncBpm::SYNC_1BEAT : SyncBpm::SYNC_2BEAT);
-
-    tm1640.init(4, 2);
-    showStepMode(mode);
-
-    MsTimer2::set(INTERNAL_PULSE_MSEC, timer2Callback);
-    Timer1.initialize();
-    Timer1.attachInterrupt(timerOneInterrupt);
-    Timer1.start();
-    switchMode(INTERNAL_MODE);
-    attachInterrupt(digitalPinToInterrupt(pin::Ext_Sync), externalSyncInterrupt, CHANGE);
-}
-
 void updateTempo7Seg(const uint16_t tempo)
 {
     uint8_t digits[4];
@@ -196,55 +210,13 @@ void updateTempo7Seg(const uint16_t tempo)
     }
 }
 
-int nudge()
-{
-    const auto minus = digitalRead(pin::NudgeMinus) == LOW ? -1 : 0;
-    const auto plus  = digitalRead(pin::NudgePlus) == LOW ? 1 : 0;
-
-    return minus + plus;
-}
-
-void timerOneInterrupt()
-{
-    if (applicationMode == INTERNAL_MODE) {
-        digitalWrite(pin::Sync_Out, HIGH);
-        Timer1.setPeriod(timerValue); // timer period must be set here to make "continuous" tempo output
-        MsTimer2::start();
-        return;
-    }
-
-    if (applicationMode == EXTERNAL_MODE) {
-        externalSyncNotCommingCounter++;
-        if (externalSyncNotCommingCounter >= stepParameter.notComingCounterMax) {
-            externalSyncNotCommingCounter = 0;
-            switchMode(INTERNAL_MODE);
-        }
-    }
-}
-
 uint32_t bpmToSyncTimerCounter(const float bpm)
 {
     const auto ret = (uint32_t)((60000000 / bpm / stepParameter.step_div) + 0.5);
     return ret;
 }
 
-void loop()
-{
-    switch (applicationMode) {
-    case INTERNAL_MODE:
-        internalMode_loop();
-        break;
-    case EXTERNAL_MODE:
-        externalMode_loop();
-        break;
-    default:
-        break;
-    }
-
-    tm1640.loop();
-}
-
-uint32_t getKnobTempo()
+uint32_t getKnobBpm()
 {
     auto knob    = tempoAdc.read();
     auto knobBpm = map(knob, 0, 1023, 3000, 24000);
@@ -271,6 +243,14 @@ float getFaderPercent()
     return faderPercent;
 }
 
+int nudge()
+{
+    const auto minus = digitalRead(pin::NudgeMinus) == LOW ? -1 : 0;
+    const auto plus  = digitalRead(pin::NudgePlus) == LOW ? 1 : 0;
+
+    return minus + plus;
+}
+
 void internalMode_loop()
 {
     const auto buttonDown = digitalRead(pin::Tap) == LOW;
@@ -284,7 +264,7 @@ void internalMode_loop()
     }
 
     auto knobBpmChanged = false;
-    auto knobBpm        = getKnobTempo();
+    auto knobBpm        = getKnobBpm();
     if (knobBpm != lastKnobBpm) {
         knobBpmChanged = true;
         lastKnobBpm    = knobBpm;
@@ -321,4 +301,54 @@ void externalMode_loop()
         lastExternalBpm = externalBpm;
         updateTempo7Seg(externalBpm);
     }
+}
+
+/***************************************
+ * Arduino's
+***************************************/
+void setup()
+{
+    for (auto&& config : digitalPinConfigs) {
+        pinMode(config.pinNo, config.mode);
+    }
+
+    Serial.begin(115200);
+    auto mode = bootMode::getBootMode();
+    Serial.print("Boot Mode: ");
+    Serial.println(bootMode::bootModeText(mode));
+
+    if (mode == bootMode::INSPECTION) {
+        startHardwareInspection();
+        mode = bootMode::NORMAL_1STEP;
+    }
+    stepMode = mode == bootMode::NORMAL_1STEP ? STEP_1 : STEP_2;
+    setStepParameter(stepParameter, stepMode);
+    Serial.println(stepMode);
+    syncBpm.setMode(stepMode == STEP_1 ? SyncBpm::SYNC_1BEAT : SyncBpm::SYNC_2BEAT);
+
+    tm1640.init(4, 2);
+    showStepMode(mode);
+
+    MsTimer2::set(INTERNAL_PULSE_MSEC, timer2Interrupt);
+    Timer1.initialize();
+    Timer1.attachInterrupt(timerOneInterrupt);
+    Timer1.start();
+    switchApplicationMode(INTERNAL_MODE);
+    attachInterrupt(digitalPinToInterrupt(pin::Ext_Sync), externalSyncInterrupt, CHANGE);
+}
+
+void loop()
+{
+    switch (applicationMode) {
+    case INTERNAL_MODE:
+        internalMode_loop();
+        break;
+    case EXTERNAL_MODE:
+        externalMode_loop();
+        break;
+    default:
+        break;
+    }
+
+    tm1640.loop();
 }
